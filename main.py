5b2f861abdd78d4bc6306cbe45aac4b35fcc32fe
vedant3620 <49477120+vedant3620@users.noreply.github.com>
2020-04-12 12:04:04 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-directory = 'C://Users//Vedant//Desktop'
-test = os.listdir( directory )
-for item in test:
-    if item.endswith(".exe"):
-        os.remove( os.path.join( directory, item ) )
